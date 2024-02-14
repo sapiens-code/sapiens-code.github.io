c0d1565acf5865eb49d0f6053f16c2b7414afe62
Hy.Lam <54999205+sapiens-code@users.noreply.github.com>
2024-02-14 22:32:14 +0700

    depoy

---
+++ build/precache-manifest.dbbdf2dc00c0948ed12ea98ba378a2d3.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7630caaf3aa9b6aca5076d3f45ddf647",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/index.html"
+  },
+  {
+    "revision": "d11743fa3989b697004f",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/css/main.b987213b.chunk.css"
+  },
+  {
+    "revision": "8c29d3c454fb09609869",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/js/2.b75f2722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/js/2.b75f2722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d11743fa3989b697004f",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/js/main.c91711fb.chunk.js"
+  },
+  {
+    "revision": "aef3fda774427e4ec4ff",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/js/runtime-main.cb5d5697.js"
+  },
+  {
+    "revision": "f2896dcba71416ba5ada830fd76a92c6",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_0069.f2896dcb.jpg"
+  },
+  {
+    "revision": "bd8fee858441714bbdffc3266fcce4e1",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_0430.bd8fee85.jpg"
+  },
+  {
+    "revision": "bd86304fd120aad5c0eeafab3c11e5fa",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_0491.bd86304f.jpg"
+  },
+  {
+    "revision": "e1a77937af4a0575bc75186611e8a16c",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_0795.e1a77937.jpg"
+  },
+  {
+    "revision": "2fa7b807f4d53e4e30585355da4c854b",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_0891.2fa7b807.jpg"
+  },
+  {
+    "revision": "26f8fa7adc7ddd00d4902dda10f54e8b",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1137.26f8fa7a.jpg"
+  },
+  {
+    "revision": "aebe624356f7bd83e6d7ed744ab51bc7",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1232.aebe6243.JPG"
+  },
+  {
+    "revision": "c2a68bf7603925e3b8d48f076c139dc9",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1247.c2a68bf7.jpg"
+  },
+  {
+    "revision": "9649ca3c8d1122ceb997cb82ef7f07f4",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1275.9649ca3c.jpg"
+  },
+  {
+    "revision": "842b3d9d859e215c6e0f649d05250b2f",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1290.842b3d9d.jpg"
+  },
+  {
+    "revision": "89dfac22a8ba0722985f3b306270f292",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1458.89dfac22.jpg"
+  },
+  {
+    "revision": "dece3287ef48142d2d9a05e9a666b43c",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1889.dece3287.JPG"
+  },
+  {
+    "revision": "60b121332c542b0baffd38af2ae2be55",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_1993.60b12133.jpg"
+  },
+  {
+    "revision": "f21b3e22ca32a996ae472b8e01659730",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_2218.f21b3e22.jpg"
+  },
+  {
+    "revision": "d89c823e8a03d011b590ec4d6310e6af",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/IMG_2255.d89c823e.jpg"
+  },
+  {
+    "revision": "b355612e1347a0a445a7f21c02719fa1",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/eddyS2Lidiane.b355612e.jpeg"
+  },
+  {
+    "revision": "d348e4401884dd1bb421bbe513fa54a7",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/pandinha.d348e440.gif"
+  },
+  {
+    "revision": "f1be026ff08aab1125b691c069d382f0",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/pandinha_coracao.f1be026f.gif"
+  },
+  {
+    "revision": "46feb314ef66fd9844e6c5343e228597",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/pandinha_fofa_cara_de_brava.46feb314.gif"
+  },
+  {
+    "revision": "8b0c4eecc314fa3131eae4e8ec70d06a",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/pandinha_martelo.8b0c4eec.png"
+  },
+  {
+    "revision": "865f1c977c2083c42cda7c223f72ea76",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinha_chorando.865f1c97.gif"
+  },
+  {
+    "revision": "c4ff310ed4399ebe2c85ffc52ad5eeab",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinha_mordidas.c4ff310e.gif"
+  },
+  {
+    "revision": "641a584c05345c733e8e5d94f906e53c",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinho_S2_pandinha.641a584c.gif"
+  },
+  {
+    "revision": "7746f73129cc8fc7f46ada5e23f4d1b5",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinho_catucando_pandinha.7746f731.gif"
+  },
+  {
+    "revision": "ee3bbcefdd762dc554b5db84df32bded",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinho_chorando.ee3bbcef.gif"
+  },
+  {
+    "revision": "bde071c432bb0a09b34af0e0d5ffcd98",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinho_estudando.bde071c4.gif"
+  },
+  {
+    "revision": "81e2399aba3f1ff6cd0c1df8eb967412",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinho_pandinha_ok.81e2399a.gif"
+  },
+  {
+    "revision": "387281264ae4000e8a803a40169c1ac8",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinho_pegando_no_sono.38728126.gif"
+  },
+  {
+    "revision": "04b613c4523e12dcb35d7f703a752faf",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinhos_apaixonados.04b613c4.gif"
+  },
+  {
+    "revision": "e831c6c0da241c5146771d0233cfc219",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinhos_bebida.e831c6c0.gif"
+  },
+  {
+    "revision": "b887f38a5ae81d5a3fe91244955386dc",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinhos_caminhada.b887f38a.jpg"
+  },
+  {
+    "revision": "73252a13157ecdec88497a19c35256e5",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinhos_dormindo.73252a13.gif"
+  },
+  {
+    "revision": "4aa4f4ad4538a66d10b1537ea06d994c",
+    "url": "//sapiens-code.github.io/sapiens-code.github.io/static/media/ursinhos_fotinha.4aa4f4ad.jpg"
+  }
+]);
